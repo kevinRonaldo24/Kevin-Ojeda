8af63e94d06adb7e4ed8db61f5353154c256f712
Your Nam <kevinronaldo04m@gmail.com>
2025-08-21 10:51:18 -0500

    sdsdoo

---
+++ asistencia/src/main/java/pe/edu/upeu/asistencia/aplication.java
@@ -0,0 +1,11 @@
+package pe.edu.upeu.asistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+
+public class aplication {
+    public static void main(String[] args) {
+        System.out.println("iniciando asistencia DMP");
+       AsistenciaApplication.main(args);
+    }
+}
