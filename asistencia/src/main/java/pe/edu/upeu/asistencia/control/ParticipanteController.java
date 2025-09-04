0084aa00b5efb9092a7903b2367a44ccaf9b779a
Your Nam <kevinronaldo04m@gmail.com>
2025-09-04 12:19:10 -0500

    Asistencia

--- asistencia/src/main/java/pe/edu/upeu/asistencia/control/ParticipanteController.java
+++ asistencia/src/main/java/pe/edu/upeu/asistencia/control/ParticipanteController.java
@@ -1,7 +1,24 @@
 package pe.edu.upeu.asistencia.control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import org.springframework.stereotype.Controller;
+import pe.edu.upeu.asistencia.enums.Carrera;
+import pe.edu.upeu.asistencia.enums.TipoParticipante;
 
 @Controller
 public class ParticipanteController {
+
+    @FXML
+    private ComboBox<Carrera> cbxcarrera;
+
+    @FXML
+    private ComboBox<TipoParticipante> cbxtipoparticipante;
+
+    @FXML
+    public void initialize(){
+        cbxcarrera.getItems().addAll(Carrera.values());
+        cbxtipoparticipante.getItems().addAll(TipoParticipante.values());
+
+    }
 }
